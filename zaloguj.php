2fd345bda3f7e0f54e83aa481560844538cce06e
Piotr Marynczak <piotr.marynczak.programista@gmail.com>
2019-06-30 12:56:26 +0200

    added protection from SQL injections

--- zaloguj.php
+++ zaloguj.php
@@ -20,9 +20,13 @@
         $login = $_POST['login'];
         $password = $_POST['password'];
         
-        $sql = "SELECT * FROM users WHERE username='$login' AND password='$password'";
+        $login = htmlentities($login, ENT_QUOTES, "UTF-8");
+		$password = htmlentities($password, ENT_QUOTES, "UTF-8");
         
-        if ($result = @$connection->query($sql)) {
+        if ($result = @$connection->query(
+        sprintf("SELECT * FROM users WHERE username='%s' AND password='%s'",
+        mysqli_real_escape_string($connection,$login),
+		mysqli_real_escape_string($connection,$password)))) {
             
             $users_amount = $result->num_rows;
             if($users_amount > 0) {
@@ -42,10 +46,9 @@
                 
                 $_SESSION['error'] = '<span style="color:red">Nieprawidłowy login lub hasło!</span>';
                 header('Location: index.php');
-                
             }
         }
-        
+
         $connection->close();
     }
 
